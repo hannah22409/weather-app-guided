319090f5e624f9104e047f53bd345ceacec889ac
hannah22409 <70591183+hannah22409@users.noreply.github.com>
2020-12-29 22:07:28 -0500

    added own icons

--- src/app.js
+++ src/app.js
@@ -42,6 +42,60 @@ function displayTemperature(response) {
     let iconElement=document.querySelector("#icon");
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description);
+    if (response.data.weather[0].icon == "01d") {
+        iconElement.setAttribute("src","images/clear sky day.png");
+    }
+    if (response.data.weather[0].icon == "01n") {
+        iconElement.setAttribute("src","images/clear sky night.png");
+    }
+    if (response.data.weather[0].icon == "02d") {
+        iconElement.setAttribute("src","images/few clouds day.png");
+    }
+    if (response.data.weather[0].icon == "02n") {
+        iconElement.setAttribute("src","images/few clouds night.png");
+       }
+    if (response.data.weather[0].icon == "03d") {
+        iconElement.setAttribute("src","images/clouds.png");
+       }
+    if (response.data.weather[0].icon == "03n") {
+        iconElement.setAttribute("src","images/clouds.png");
+       }
+    if (response.data.weather[0].icon == "04d") {
+        iconElement.setAttribute("src","images/double clouds.png");
+       }
+    if (response.data.weather[0].icon == "04n") {
+        iconElement.setAttribute("src","images/double clouds.png");
+    }
+    if (response.data.weather[0].icon == "09d") {
+        iconElement.setAttribute("src","images/heavy rain.png");
+       }
+    if (response.data.weather[0].icon == "09n") {
+        iconElement.setAttribute("src","images/heavy rain.png");
+       }
+    if (response.data.weather[0].icon == "10d") {
+        iconElement.setAttribute("src","images/rain day.png");
+       }
+    if (response.data.weather[0].icon == "10n") {
+        iconElement.setAttribute("src","images/rain night.png");
+       }
+    if (response.data.weather[0].icon == "11d") {
+        iconElement.setAttribute("src","images/thunderstorms.png");
+       }
+    if (response.data.weather[0].icon == "11n") {
+        iconElement.setAttribute("src","images/thunderstorms.png");
+       }
+    if (response.data.weather[0].icon == "13d") {
+        iconElement.setAttribute("src","images/snow.png");
+       }
+    if (response.data.weather[0].icon == "13n") {
+        iconElement.setAttribute("src","images/snow.png");
+       }
+    if (response.data.weather[0].icon == "50d") {
+        iconElement.setAttribute("src","images/clouds.png");
+       }
+    if (response.data.weather[0].icon == "50n") {
+        iconElement.setAttribute("src","images/clouds.png");
+       }
 }
 
 let apiKey = "19e93f29b7b85bee7efc4d2a5126ad21";
